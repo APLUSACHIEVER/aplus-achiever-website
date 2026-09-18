dc97f0739dca66aa84b5d10371d6ae365a89832b
APLUSACHIEVER <kenning689@gmail.com>
2026-09-18 14:27:40 +0800

    Create APLUS PSLE Science practice site foundation

---
+++ science/paper-index.js
@@ -0,0 +1,6 @@
+window.APLUS_SCIENCE_PAPERS=[
+/* Add authorised/user-provided papers here.
+Example:
+{id:"ACS-2026",school:"ACS Junior",year:2026,paper:"P6 Science Prelim",file:"papers/acs-junior-2026.pdf"}
+*/
+];
