16eef071e8eb3453d3c0555119192b0d6f681e27
APLUSACHIEVER <kenning689@gmail.com>
2026-09-18 08:45:38 +0800

    Switch to Visual Text DNA V3

--- prelim-generation-quality-gate-v2-20260918.js
+++ prelim-generation-quality-gate-v2-20260918.js
@@ -23,7 +23,7 @@ install();let tries=0;const poll=setInterval(()=>{if(install()||++tries>80)clear
 (function(){'use strict';
 const loaders=[
  ['grammar-cloze','prelim-psle-dna-grammar-cloze-v1-20260918.js','20260918c'],
- ['visual-text','prelim-psle-dna-visual-text-v2-20260918.js','20260918a'],
+ ['visual-text','prelim-psle-dna-visual-text-v3-20260918.js','20260918a'],
  ['editing','prelim-psle-dna-editing-v2-20260918.js','20260918a'],
  ['comprehension-cloze','prelim-psle-dna-comprehension-cloze-v1-20260918.js','20260918c'],
  ['comprehension-oe','prelim-psle-dna-comprehension-oe-v1-20260918.js','20260918b']
